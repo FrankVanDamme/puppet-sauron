6f8b2d3859bdfea05209acde180411efde0715f1
Frank Van Damme <frank.van.damme@vub.be>
2023-11-08 14:40:26 +0100

    cosmetics

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,30 +11,29 @@ class sauron::params () {
     $saurontag          = $::environment
     $eye                = {}
     $config = {
-	dirs => {
-	  # directory where log files are kept
-	  log => "/home/sauron/sauron2.d/logs",
-	  # directory where tmp files are kept
-	  tmp => "/home/sauron/sauron2.d/tmp",
-	},
-	# send mail
-	email => {
-	  enabled => true,
-	  services => true,
-	},
-	# mounts which are ignored
-	ignored => {} ,
-	notify => [
-	],
-	ssh_timeout => "15",
-	thresholds => {
-	    "default" => {
-	      info => 87,
-	      notice => 90,
-	      warning => 94,
-	      critical => 97,
-	    },
-	}
-    } 
+    dirs => {
+      # directory where log files are kept
+      log => "/home/sauron/sauron2.d/logs",
+      # directory where tmp files are kept
+      tmp => "/home/sauron/sauron2.d/tmp",
+    },
+    # send mail
+    email => {
+        enabled => true,
+        services => true,
+    },
+    # mounts which are ignored
+    ignored => {} ,
+    notify => [
+    ],
+    ssh_timeout => "15",
+    thresholds => {
+        "default" => {
+            info => 87,
+            notice => 90,
+            warning => 94,
+            critical => 97,
+        },
+    }
+    }
 }
-
--- manifests/server/skeleton.pp
+++ manifests/server/skeleton.pp
@@ -1,15 +1,16 @@
+# Creates directories
 class sauron::server::skeleton (
 ) inherits sauron::params {
     $ensure_dir = $ensure? {
-	present => directory,
-	default => $ensure,
+        present => directory,
+        default => $ensure,
     }
 
-    file { 
-        [ 
-            "/home/sauron/sauron2.d", 
-            "/home/sauron/bin", 
-            "/home/sauron/sauron2.d/logs", 
+    file {
+        [
+            "/home/sauron/sauron2.d",
+            "/home/sauron/bin",
+            "/home/sauron/sauron2.d/logs",
             "/home/sauron/sauron2.d/tmp" ] :
         ensure => $ensure_dir,
         owner  => "sauron",
